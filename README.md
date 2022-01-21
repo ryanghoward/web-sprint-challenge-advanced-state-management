9742a53d16708d25204a91625ee2656333d6ba35
Ryan Howard <ryanghoward@protonmail.com>
2022-01-21 10:51:50 -0800

    Smurfette data is rendering to DOM

--- README.md
+++ README.md
@@ -83,16 +83,16 @@ Add in the action creators and action constants needed to add a smurf to state a
 
 Connect component to the fetchSmurfs action.
 
-- [ ] Connect the fetchSmurfs actions to the App component.
-- [ ] Call the fetchSmurfs action when the component first loads.
+- [x] Connect the fetchSmurfs actions to the App component.
+- [x] Call the fetchSmurfs action when the component first loads.
 
 ### Complete components/SmurfList.js
 
 Connect this component to your smurfs and loading screen state slices.
 
-- [ ] Connect the smurfs and loading state values to the SmurfList component.
-- [ ] Replace the single Smurf component instance with a map return a Smurf component for each entry in the smurfs list.
-- [ ] Replace the static isLoading variable with the state loading variable.
+- [x] Connect the smurfs and loading state values to the SmurfList component.
+- [x] Replace the single Smurf component instance with a map return a Smurf component for each entry in the smurfs list.
+- [x] Replace the static isLoading variable with the state loading variable.
 
 ### Complete components/AddForm.js
 
--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import Smurf from './Smurf';
+import React from "react";
+import Smurf from "./Smurf";
+import { connect } from "react-redux";
 
- const SmurfList = ()=> {
-    const isLoading = false;
-    const testSmurf = {
-        id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-        name:'Poppa Smurf',
-        position:'Village Leader',
-        nickname: 'Pops',
-        description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
-    }
+const SmurfList = (props) => {
+  const { smurf, isLoading } = props;
 
-    if (isLoading) {
-        return <h1>Loading...</h1>;
-    }
+  // const isLoading = false;
+  // const testSmurf = {
+  //     id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+  //     name:'Poppa Smurf',
+  //     position:'Village Leader',
+  //     nickname: 'Pops',
+  //     description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
+  // }
 
-    return(<div className="listContainer">
-        <Smurf smurf={testSmurf}/>
-    </div>);
-}
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
-export default SmurfList;
+  return (
+    <div className='listContainer'>
+      {smurf.map((testSmurf) => (
+        <Smurf smurf={testSmurf} />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    smurf: state.smurf,
+    isLoading: state.isLoading,
+    error: state.error,
+  };
+};
+
+export default connect(mapStateToProps)(SmurfList);
 
 //Task List:
 //1. Connect the smurfs and loading state values to the SmurfList component.
