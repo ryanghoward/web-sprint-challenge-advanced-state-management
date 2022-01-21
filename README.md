65ad8b6d11d974b8203210346f549507fb3c7b7f
Ryan Howard <ryanghoward@protonmail.com>
2022-01-21 10:21:42 -0800

    Initial state set and reducers made

--- README.md
+++ README.md
@@ -52,7 +52,7 @@ In this project, you will build the reducer, actions and basic redux connects to
 
 ### Complete reducers/index.js
 
-Add in the needed state and reducer cases to hold and modify smurf error messages, loading status and smurf data. **If at all possible, add in action cases one at a time, instead of all at once. Test your state connects and reducer cases as nessisary.**
+Add in the needed state and reducer cases to hold and modify smurf error messages, loading status and smurf data. **If at all possible, add in action cases one at a time, instead of all at once. Test your state connects and reducer cases as necessary.**
 
 - [ ] Adds the following state values into the initialState:
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,61 @@
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAIL, ADD_SMURF } from "../actions";
 
 export const initialState = {
-}
+  smurf: [
+    {
+      id: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+      name: "Poppa Smurf",
+      position: "Village Leader",
+      nickname: "Pops",
+      description:
+        "Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.",
+    },
+  ],
+  isLoading: false,
+  error: "Ya done goofed",
+};
 
-const reducer = ()=>{
-}
+const reducer = (state = initialState, action) => {
+  console.log(action.payload, "actions");
+
+  switch (action.type) {
+    case FETCH_START:
+      return {
+        ...state,
+        smurf: [],
+        isLoading: true,
+        error: "",
+      };
+    case FETCH_SUCCESS:
+      return {
+        smurf: action.payload,
+        isLoading: false,
+        error: "",
+      };
+    case FETCH_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+    case ADD_SMURF:
+      return {
+        smurf: [
+          ...state.smurf,
+          {
+            name: action.payload.name,
+            position: action.payload.position,
+            nickname: action.payload.nickname,
+            description: action.payload.description,
+          },
+        ],
+        isLoading: false,
+        error: "",
+      };
+    default:
+      return state;
+  }
+};
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
 export default reducer;
@@ -15,8 +67,8 @@ export default reducer;
 //  - a string indicating a possible error message
 
 //2. Add in the arguments needed to complete a standard reducer function.
-//3. Add in a reducer case to accomidate the start of a smurf fetch.
-//4. Add in a reducer case to accomidate the successful smurf api fetch.
-//5. Add in a reducer cases to accomidate the failed smurf api fetch.
-//6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
+//3. Add in a reducer case to accommodate the start of a smurf fetch.
+//4. Add in a reducer case to accommodate the successful smurf api fetch.
+//5. Add in a reducer cases to accommodate the failed smurf api fetch.
+//6. Add in a reducer case to accommodate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
 //7. Add in a reducer case that adds in a value to the error message.
