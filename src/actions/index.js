4b9395a753bd1a8866485b2292eb3cd4ee44cbfb
Ryan Howard <ryanghoward@protonmail.com>
2022-01-21 09:53:52 -0800

    Actions made

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
 //3. Add a standard action that allows us to set the value of the error message slice of state.
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+export const ADD_SMURF = "ADD_SMURF";
+
+export const fetchStart = () => {
+  return ({type: FETCH_START});
+}
+
+export const fetchSuccess = (smurf) => {
+  return ({type: FETCH_SUCCESS, payload: smurf};)
+}
+
+export const fetchFail = (err) => {
+  return ({type: FETCH_FAIL, payload: err})
+}
+
+export const addSmurf = (item) => {
+  return ({type: ADD_SMURF, payload: item};)
+}
